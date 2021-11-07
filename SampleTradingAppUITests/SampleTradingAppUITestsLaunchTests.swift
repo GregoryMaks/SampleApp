23596817ee9712200adc0cb254d7b86bd6dfc238
Gregory Maksiuk <gregory@macpaw.com>
2021-11-07 18:17:33 +0200

    Initial Commit

---
+++ SampleTradingAppUITests/SampleTradingAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SampleTradingAppUITestsLaunchTests.swift
+//  SampleTradingAppUITests
+//
+//  Created by Gregory Maksiuk on 07.11.2021.
+//
+
+import XCTest
+
+class SampleTradingAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
