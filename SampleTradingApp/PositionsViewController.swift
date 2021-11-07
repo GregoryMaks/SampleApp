1a437534883fcd296fdf3e16c55f9bdc99357441
Gregory Maksiuk <gregory@macpaw.com>
2021-11-07 18:42:28 +0200

    Add tableView

---
+++ SampleTradingApp/PositionsViewController.swift
@@ -0,0 +1,75 @@
+//
+//  PositionsViewController.swift
+//  SampleTradingApp
+//
+//  Created by Gregory Maksiuk on 07.11.2021.
+//
+
+import UIKit
+
+final class PositionsViewController: UIViewController {
+
+    var tableView: UITableView!
+
+    override var navigationItem: UINavigationItem {
+        let item = UINavigationItem(title: "TODO")
+        item.rightBarButtonItem = UIBarButtonItem(
+            image: .init(systemName: "line.3.horizontal"),
+            style: .plain,
+            target: self,
+            action: #selector(contextMenuButtonClicked(_:))
+        )
+        return item
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTableView()
+    }
+}
+
+private extension PositionsViewController {
+
+    func setupTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension PositionsViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO
+        0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO
+        preconditionFailure()
+    }
+}
+
+extension PositionsViewController: UITableViewDelegate {
+}
+
+// MARK: - Actions
+
+extension PositionsViewController {
+
+    @objc
+    func contextMenuButtonClicked(_ sender: Any) {
+    }
+}
